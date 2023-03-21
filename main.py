9dfb45db6acd59962424cc983e7908ac2d08d157
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2023-03-21 13:02:47 -0300

    :hammer:refactor: Added schema to Items

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 from db.memory import db
 from id_generator import IDGenerator
+from datetime import datetime
 
 app = FastAPI()
 id_generator = IDGenerator()
+
+
+
+class Item(BaseModel):
+    item_id: int = Field(default_factory=id_generator.get_next_id, editable=False) 
+    title: str
+    checked: bool = Field(default=False, editable=False)
+    created_at: str = Field(default_factory=lambda: datetime.now().isoformat(), editable=False)
+    deleted_at: str = Field(default=None, editable=False)
+    updated_at: str = Field(default=None, editable=False)
 
 @app.get("/")
 async def root():
     return db.list()
 
 @app.post("/")
-async def create_item(item: dict, status_code=201):
+async def create_item(item: Item, status_code=201):
     try: 
-        db.add({ "id": id_generator.get_next_id(), **item})
+        db.add(item)
         # object = { "id": id_generator.get_next_id(), **item}
         # print(object)
         return { 'message': 'Item created' }
