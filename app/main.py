83dfabbdd080e014f5046d9e76ee08109876bf1c
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2023-03-23 16:15:12 -0300

    :hammer:refactor: add better error handling
    Add put route

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+import uuid
+from fastapi import FastAPI, HTTPException
 from db.memory import db
 from id_generator import IDGenerator
 from models.item import Item
@@ -17,6 +18,16 @@ async def create_item(item: Item):
         db.add(item)
         return { 'message': 'Item created' }
     except Exception as e:
-        print(e)
         status_code = 500
-        return { 'message': 'Error creating item' }
+        error_message =  { 'message': 'Error creating item',  'error': str(e)}
+        raise HTTPException(status_code=status_code, detail=error_message)
+    
+@app.put("/{item_id}")
+async def update_item(item_id: uuid, item: Item):
+    try:
+        db.update(item_id, item)
+        return { 'message': 'Item updated' }
+    except Exception as e:
+        status_code = 500
+        error_message =  { 'message': 'Error updating item', 'error': str(e)}
+        raise HTTPException(status_code=status_code, detail=error_message)
