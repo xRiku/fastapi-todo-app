0dac7da521d263dc24137ca78faacbfd604ff638
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2023-03-30 17:03:44 -0300

    :rotating_light:test: add verify if id is inside db assetion

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 from uuid import UUID
 from app.main import app
+from db.memory import db
 
 client = TestClient(app)
 
@@ -12,6 +13,7 @@ def test_create_item():
             "title": "Test item",
         },
     )
-    assert response.status_code == 200
-    assert response.json() == {"message": "Item created"}
 
+    assert response.status_code == 201
+    assert response.json() == {"message": "Item created", "item": {"id": response.json()["item"]["id"], "title": "Test item", "checked": False, "created_at": response.json()["item"]["created_at"], "deleted_at": None, "updated_at": None, "position": 1}}
+    assert db.is_inside(UUID(response.json()["item"]["id"])) == True
